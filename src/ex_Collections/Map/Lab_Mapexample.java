0ac4635a701e61e47ca88cf991b49c4940b870d7
Pratikshagup <guptapratiksha2412@gmail.com>
2025-07-19 22:06:34 +0530

    commit 19 july

---
+++ src/ex_Collections/Map/Lab_Mapexample.java
@@ -0,0 +1,32 @@
+package ex_Collections.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lab_Mapexample {
+    public static void main(String[] args) {
+        Map<String,Integer> vehicle = new HashMap();
+        vehicle.put("MG Aastror",1);
+        vehicle.put("i10",2);
+        vehicle.put("Hornet",2);
+        vehicle.put("BMW",1);
+        vehicle.put("Tesla",5);
+        System.out.println("Total Vehicles: " +vehicle.size()); //number of keys
+
+        //Iterator for Map
+        for(String key : vehicle.keySet()){
+            System.out.println(key+" -> " +vehicle.get(key));
+        }
+
+        //Checking for a key
+        if(vehicle.containsKey("Hornet")){
+            System.out.println("It exists!");
+        }
+        else System.out.println("It doesn't exists!");
+
+        //clearing map
+        vehicle.clear();
+        System.out.println("Total Vehicles: " +vehicle.size()); //updated count
+    }
+
+}
---
+++ src/ex_Collections/Map/Map1.java
@@ -0,0 +1,28 @@
+package ex_Collections.Map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Map1 {
+    public static void main(String[] args) {
+        Map m1 = new HashMap(10);
+        m1.put("name","Pratiksha");
+        m1.put("rollno",1);
+        m1.put("phone",958879567);
+        System.out.println(m1);
+
+        Map m2 = new LinkedHashMap();
+        m2.put("name","Prateek");
+        m2.put("rollno",2);
+        m2.put("phone",968879567);
+        System.out.println(m2);
+
+       Map m3 = new TreeMap();
+        m3.put("name","Prachi");
+        m3.put("rollno",3);
+        m3.put("phone",978879567);
+        System.out.println(m3);
+    }
+}
